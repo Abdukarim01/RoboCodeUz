d519103a1e2d1c7b3c6728877440bb675653742c
Abdukarim01 <84226194+Abdukarim01@users.noreply.github.com>
2022-02-09 18:57:55 -0800

    new commit

--- staticfiles/js/local/main.js
+++ staticfiles/js/local/main.js
@@ -1,31 +1,34 @@
 /* START NAVBAR CONTROLR */
 
+let navnum = 0;
+
 function controlnavbar() {
- function media1(x) {
-  let line = document.querySelectorAll(".line");
-  var navbar = document.querySelector('.navbar_right');
-  if (x.matches) { 
-        if (navbar.clientHeight) {
-        navbar.style.height = 0;
-        line.forEach( function(e){
-          e.classList = "line"
-        })
-      }else{
-        var wrapper = navbar.querySelector("ul")
-        navbar.style.height = wrapper.clientHeight + "px";
-        line.forEach( function(e){
-          e.classList = "line for_navbar_control"
-        })
+   navnum++
+   function media1(x) {
+      let line = document.querySelectorAll(".line");
+      var navbar = document.querySelector('.navbar_right');
+      if (x.matches) { 
+            if(navnum >= 2){navnum = 0}
+            if (navnum == 0) {
+                  navbar.style.height = 0;
+                  line.forEach( function(e){
+                    e.classList = "line"
+                  })
+            }if(navnum == 1){
+                  var wrapper = navbar.querySelector("ul")
+                  navbar.style.height = wrapper.clientHeight + "px";
+                  line.forEach( function(e){
+                    e.classList = "line for_navbar_control"
+                  })
+          }
+      } else {
+          navnum = 0
+          navbar.style.height = "";
       }
-  } else {
-      navbar.style.height = "";
   }
-}
-var x = window.matchMedia("(max-width: 992px)")
-media1(x) 
-x.addListener(media1)
+  var x = window.matchMedia("(max-width: 992px)")
+  media1(x) 
+  x.addListener(media1)
 }
 
 /* END NAVBAR CONTROLR */
-
-

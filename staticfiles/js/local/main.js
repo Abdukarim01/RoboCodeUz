d263b6c7ebe9d4b5a1e3dba37c8f6e95f4df7604
Abdukarim01 <84226194+Abdukarim01@users.noreply.github.com>
2022-02-16 02:22:40 -0800

    new commit

--- staticfiles/js/local/main.js
+++ staticfiles/js/local/main.js
@@ -47,52 +47,46 @@ var typerslide = new Swiper(".typer_slide", {
 
 /* COURSES ANIMATE */
 
+window.addEventListener('scroll', function() {
+    let course = document.querySelectorAll(".course_container")
+    let res = (window.innerHeight -  course[0].clientHeight) / 2
+    let position = course[0].getBoundingClientRect()
+    if(position.top <= window.innerHeight - (res + course[0].clientHeight - 10)){
+        course[1].style = `position: relative; margin-top:-${course[0].clientHeight - 40}px; transition: all 0.5s; z-index: 10;`
+    }
+    else{
+        course[1].style = `position: relative;  transition:all 0.5s; z-index: 10;`
+    }
 
+    if(course[1].getBoundingClientRect().top <= window.innerHeight - (res + course[1].clientHeight - 40)){
+        course[2].style = `position: relative; margin-top:-${course[1].clientHeight - 40}px; transition: all 0.5s; z-index: 10;`
+    }
+    else{
+        course[2].style = `position: relative;  transition:all 0.5s; z-index: 10;`
+    }
 
+    if(course[2].getBoundingClientRect().top <= window.innerHeight - (res + course[2].clientHeight - 50)){
+        course[3].style = `position: relative; margin-top:-${course[2].clientHeight - 40}px; transition: all 0.5s; z-index: 10;`
+    }
+    else{
+        course[3].style = `position: relative;  transition:all 0.5s; z-index: 10;`
+    }
 
+    if(course[3].getBoundingClientRect().top <= window.innerHeight - (res + course[3].clientHeight - 40)){
+        course[4].style = `position: relative; margin-top:-${course[3].clientHeight - 40}px; transition: all 0.5s; z-index: 10;`
+    }
+    else{
+        course[4].style = `position: relative;  transition:all 0.5s; z-index: 10;`
+    }
 
-// window.addEventListener('scroll', function() {
-//             function animfunctionmedia(y) {
-//                     if (y.matches) {  
-//                         let element = document.querySelectorAll(".course_container");
-//                         element.forEach( (e)=>{
-//                         var position = e.getBoundingClientRect();
-//                         // checking for partial visibility
-//                         if(position.top < window.innerHeight - 250) {
-//                         e.style = "transform:translateX(0%); opacity: 1;"
-//                         }else{
-//                         e.style = ''
-//                         }
-
-//                         })
-//                     } 
-//             }  
-
-//             /* FOR MEDIA */
-
-//             var y = window.matchMedia("(max-width: 768px)")
-//             animfunctionmedia(y) 
-//             y.addListener(animfunctionmedia)
-
-//             function animfunctionmedia2(z) {
-//                     if (z.matches) {  
-//                         let element = document.querySelectorAll(".course_container");
-//                         element.forEach( (e)=>{
-//                         var position = e.getBoundingClientRect();
-//                         // checking for partial visibility
-//                         if(position.top < window.innerHeight - 200) {
-//                         e.style = "transform:translateX(0%); opacity: 1;"
-//                         }else{
-//                         e.style = ''
-//                         }
-
-//                         })
-//                     } 
-//             }  
-
-//             var z = window.matchMedia("(min-width: 768px)")
-//             animfunctionmedia2(z) 
-//             z.addListener(animfunctionmedia2)
-// });
+    if(course[4].getBoundingClientRect().top <= window.innerHeight - (res + course[4].clientHeight + 100)){
+        course[5].style = `position: relative; margin-top:-${course[4].clientHeight - 40}px; transition: all 0.5s; z-index: 10;`
+    }
+    else{
+        course[5].style = `position: relative;  transition:all 0.5s; z-index: 10;`
+    }
+    
+    
+});
 
 /* ENS COURSES ANIMATE*/

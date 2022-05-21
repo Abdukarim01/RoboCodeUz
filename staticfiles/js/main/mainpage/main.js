caaf00e05e8ab1c648869bc8f7bb706df7de5289
Abdukarim01 <84226194+Abdukarim01@users.noreply.github.com>
2022-05-21 06:58:35 -0700

    new commit

--- staticfiles/js/main/mainpage/main.js
+++ staticfiles/js/main/mainpage/main.js
@@ -1,3 +1,4 @@
+/* start navbar functions */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
@@ -42,3 +43,51 @@ function navbarControlr(){
   media1(x) 
   x.addListener(media1)
 }
+/* end navbar functions */
+
+
+/* start swipers */
+let course_swiper = new Swiper(".course_swiper",{
+  loop:true
+})
+let delay = q(".typer").getAttribute("data-delay")
+let delay_delete =  q(".typer").getAttribute("data-deleteDelay")
+let words = q(".course_typer").querySelector(".typer").getAttribute("data-words").split(",").filter((v) => v)
+course_swiper.on('slideChangeTransitionEnd', function() {
+  let index_slide = course_swiper.realIndex;
+  let typer = q(".course_typer");
+  let arr = []
+  for(let i = 0; i<words.length; i++){
+    if(i !== index_slide){
+      arr.push(words[i])
+    }
+  }
+  arr.unshift(words[index_slide])
+   typer.innerHTML = `
+    <span 
+         class="typer" id="main"
+         data-words="${arr}"
+         data-delay="${delay}" data-deleteDelay="${delay_delete}">
+    </span>  
+    <span class="cursor" data-owner="main" data-cursorDisplay="|"></span>
+   `
+   TyperSetup();
+});
+
+let typerSlideNum = -1;
+function typerSlide(){
+  typerSlideNum ++
+  let content = words[typerSlideNum]
+  let wait = (q(".typer").getAttribute("data-delay") * 2) * (content.length + 1) + Number(q(".typer").getAttribute("data-deleteDelay"))
+  setTimeout(function(){
+    if(typerSlideNum < words.length-1){
+        console.log(content,wait)
+        course_swiper.slideTo(typerSlideNum+2)
+        typerSlide()
+    }
+  },wait)
+  
+  if(typerSlideNum >= words.length-1) typerSlideNum = -1
+}
+typerSlide()
+/* end swipers*/

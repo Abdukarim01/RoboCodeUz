1342b93a5395ce632c82c0ed9c6db3a2fa9ccdb6
Abdukarim01 <84226194+Abdukarim01@users.noreply.github.com>
2022-06-19 00:13:15 -0700

    new commit

--- staticfiles/js/main/mainpage/main.js
+++ staticfiles/js/main/mainpage/main.js
@@ -240,3 +240,10 @@ function asks(e){
 
   }
 }
+
+
+// fetch('url here', {
+//     method: 'POST',
+//     headers: {'Content-Type':'application/x-www-form-urlencoded'}, // this line is important, if this content-type is not set it wont work
+//     body: 'foo=bar&blah=1'
+// });
